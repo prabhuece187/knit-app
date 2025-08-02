0ef08b3c46985ddf8ceafd467c5c53387f253695
Nagarjunl <naga.career.at@gmail.com>
2025-08-02 14:20:22 +0530

    Altered font family for app with multiple fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ["var(--font-sans)", "sans-serif"],
+        // Or add as a custom font family:
+        // sans: ["Inter", "sans-serif"],
+        // mono: ["JetBrains Mono", "monospace"],
+        sans: ["var(--font-sans)"], // Bitcount
+        display: ["var(--font-display)"], // Playwrite
+      },
+    },
+  },
+};
